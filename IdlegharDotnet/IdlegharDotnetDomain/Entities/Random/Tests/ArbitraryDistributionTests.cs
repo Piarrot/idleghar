cd158de633b760f48e94f8fe126ca6d80fcb729c
Pablo Baleztena <pablo@ultharsoftware.com>
2023-02-01 14:50:39 -0300

    Add Moq mocking utilities

--- IdlegharDotnet/IdlegharDotnetDomain/Entities/Random/Tests/ArbitraryDistributionTests.cs
+++ IdlegharDotnet/IdlegharDotnetDomain/Entities/Random/Tests/ArbitraryDistributionTests.cs
@@ -1,37 +1,16 @@
+using IdlegharDotnetDomain.Providers;
+using Moq;
 using NUnit.Framework;
 
 namespace IdlegharDotnetDomain.Entities.Random.Tests
 {
     public class RandomValueFromChancesTests
     {
-        class MockRandomnessProvider : Providers.IRandomnessProvider
-        {
-            public MockRandomnessProvider(double value)
-            {
-                Value = value;
-            }
-
-            private double Value;
-
-            public double GetRandomDouble(double min, double max)
-            {
-                return Value;
-            }
-
-            public int GetRandomInt(int min, int max)
-            {
-                return 0;
-            }
-
-            public T ResolveOne<T>(RandomValue<T> rndValue)
-            {
-                return rndValue.ResolveOne(this);
-            }
-        }
-
         [Test]
         public void ItShouldProperlyResolveChances()
         {
+            var rngProviderMock = new Mock<IRandomnessProvider>();
+
             ArbitraryDistribution<string> valueFromChances = new()
             {
                 ["hello"] = 0.6,
@@ -39,13 +18,15 @@ namespace IdlegharDotnetDomain.Entities.Random.Tests
                 ["world"] = 0.1
             };
 
-            var result = valueFromChances.ResolveOne(new MockRandomnessProvider(0.5));
+            rngProviderMock.SetupSequence(x => x.GetRandomDouble(0, 1)).Returns(0.5).Returns(0.7).Returns(0.9);
+
+            var result = valueFromChances.ResolveOne(rngProviderMock.Object);
             Assert.That(result, Is.EqualTo("hello"));
 
-            result = valueFromChances.ResolveOne(new MockRandomnessProvider(0.7));
+            result = valueFromChances.ResolveOne(rngProviderMock.Object);
             Assert.That(result, Is.EqualTo("banana"));
 
-            result = valueFromChances.ResolveOne(new MockRandomnessProvider(0.9));
+            result = valueFromChances.ResolveOne(rngProviderMock.Object);
             Assert.That(result, Is.EqualTo("world"));
         }
     }
